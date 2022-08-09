30ddf7d02074e0fc425fc07858e2413fa13c3c8d
VerwinKwa@SMU <verwin.kwa.2019@sis.smu.edu.sg>
2022-08-09 13:31:03 +0800

    Update QuizEntity.py

--- Quiz/entity/QuizEntity.py
+++ Quiz/entity/QuizEntity.py
@@ -11,3 +11,20 @@ class Quiz(SQLModel, table=True):
     # Needed for Column(JSON)
     class Config:
         arbitrary_types_allowed = True
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "title": "Star Wars Quiz",
+                "category": "Movies",
+                "questions": {
+                    "question_1" : {
+                        "question" : "Who is Luke's Father?",
+                        "option_1": "Emperor Palpatine",
+                        "option_2": "Yoda",
+                        "option_3": "Han Solo",
+                        "option_4": "Chewbacca",
+                        "answer": "Anakin Skywalker"
+                    }
+                },
+            }
+        }

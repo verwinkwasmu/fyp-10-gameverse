0921152e48ed4564746659e67189216ab18fc021
verwinkwasmu <verwin.kwa.2019@scis.smu.edu.sg>
2022-08-19 14:34:39 +0800

    Update PlayerEntity.py

--- Player/entity/PlayerEntity.py
+++ Player/entity/PlayerEntity.py
@@ -6,7 +6,8 @@ class Player(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     email: str
-    points: int
+    total_points: int
+    # category_points: Dict = Field(default={}, sa_column=Column(JSON))
     categories_played: Dict = Field(default={}, sa_column=Column(JSON))
 
     # Needed for Column(JSON)
@@ -20,8 +21,8 @@ class Player(SQLModel, table=True):
                 "name": "Toh Wei",
                 "email": "tohwei@xiang.com",
                 "categories_played": {
-                    "Movies" : 1,
-                    "Kdrama" : 5,                    
+                    "Movies": {"count": 5, "points": 50},
+                    "Kdrama": {"count": 2, "points": 20},
                 },
             }
         }

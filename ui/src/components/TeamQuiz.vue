b68a9f1e4097933843ae6f7d571f092c29ecce37
Ambrose Wang <80197561+ambrosemorph@users.noreply.github.com>
2022-09-13 18:09:19 +0800

    Update TeamQuiz.vue

--- ui/src/components/TeamQuiz.vue
+++ ui/src/components/TeamQuiz.vue
@@ -1,45 +1,88 @@
 <template lang="">
-  <p class="flex justify-left text-5xl text-white pt-10 pl-20 font-light">
-    Gameverse Quiz
-  </p>
-  <div class="flex items-center justify-center pt-40 mb-0">
-    <p class="text-2xl text-white font-bold pb-5">
+
+    <!-- <p class="flex justify-left text-5xl text-white pt-10 pl-20 font-light">
+      Gameverse Quiz
+    </p> -->
+    <div class="grid grid-rows-2 grid-flow-col gap-2 justify-start">
+            <div class="text-5xl font-semibold col-span-2">GameVerse</div>
+            <div class="text-2xl col-span-2">Quiz Category</div>
+            <div class="text-sm row-span-2 flow-root">
+              <p class="float-right mt-10 text-2xl text-white font-bold">You have been invited to play the Movies Quiz!</p>
+            </div>
+      </div>
+    <div class="flex items-center justify-center pt-10 mb-0">
+    <!-- <p class="text-2xl text-white font-bold pb-5">
       You have been invited to play the Movies Quiz!
+    </p> -->
+    
+    </div>
+    <div class="flex items-center justify-center pt-0 pb-0 mb-0">
+    <p class="text-2xl text-white font-bold">
+      Select Team
     </p>
-  </div>
-  <div class="flex items-center justify-center pt-10 pb-0 mb-0">
-    <p class="text-2xl text-white font-bold">Select Team</p>
-  </div>
-  <div class="flex items-center justify-center space-x-20 m-20 pr-20 pl-20">
-    <button
-      class="w-full h-40 text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-400"
-    >
+    </div>
+    <div class="flex items-center justify-center space-x-20 mt-10 mb-20 pr-20 pl-20">
+  
+    <button @onclick ="JoinBlue" class="w-full h-40 text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-400">
+      <div class="font-bold">
       Join Blue Team
+      </div>
+      <div class="items-left">
+        <ul id="example-1">
+          <li v-for="item in items" :key="item.message">
+            {{ item.message }}
+          </li>
+        </ul>
+      </div>
     </button>
-    <button
-      class="w-full h-40 text-indigo-100 transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline hover:bg-pink-400"
-    >
+    <button @onclick ="JoinRed" class="w-full h-40 text-indigo-100 transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline hover:bg-pink-400">
+      <div class="font-bold">
       Join Red Team
+      </div>
+      <div class="items-left">
+        <ol>
+          <li>1. Peter Tan</li>
+          <li>2. Jane Liu</li>
+          <li></li>
+        </ol>
+      </div>
     </button>
-  </div>
-  <div class="flex items-center justify-between space-x-40 m-20 pt-20">
-    <button
-      class="items-centre pl-20 pr-20 h-20 text-indigo-100 border border-black bg-pink-300 rounded-none focus:shadow-outline"
-    >
+    </div>
+    <div class="flex items-center justify-between space-x-40 m-20 pt-0">
+  
+    <button class="items-centre pl-20 pr-20 h-20 text-black border border-black bg-pink-300 rounded-lg focus:shadow-outline">
       Exit Game
     </button>
-    <button
-      class="items-centre pl-20 pr-20 h-20 text-black bg-green-300 rounded-none focus:shadow-outline"
-    >
+    <button class="items-centre pl-20 pr-20 h-20 text-black bg-green-300 rounded-lg focus:shadow-outline">
       30s till start!
     </button>
-  </div>
+    </div>
+
 </template>
 
 <style>
-body {
-  background-image: url("../assets/bg.png");
-  background-repeat: no-repeat;
-  background-size: cover;
-}
+  body {
+    background-image: url('../assets/background.jpeg');
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
 </style>
+
+<script>
+export default {
+  watch: {
+    timerCount: {
+      handler(value) {
+        if (value > 0 && this.timerEnabled) {
+          setTimeout(() => {
+            this.timerCount = this.timerCount - 1;
+          }, 3000);
+        } else {
+          // this.$router.push(TeamQuizStart) // run your function here *build start page first then comment back in
+        }
+      },
+      immediate: true
+    },
+  },
+}
+</script>

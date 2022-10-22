dd974d1ff5d49041acc094f8a8f81c8d4ce975d2
Ambrose Wang <80197561+ambrosemorph@users.noreply.github.com>
2022-10-22 11:46:41 +0800

    Update QuizCreation.vue

--- ui/src/components/QuizCreation.vue
+++ ui/src/components/QuizCreation.vue
@@ -150,6 +150,8 @@ function ErrorHandling(question) {
                   >20</label
                 >
               </div>
+              <h3 v-if="store.question.timer===undefined"
+              class="ml-2 text-sm font-medium text-white"  > Please enter a time timit for this question.</h3>
             </div>
           </div>
           <div
@@ -279,7 +281,7 @@ function ErrorHandling(question) {
             <!-- <button onclick="ErrorHandling(question)" type="button" class="text-white bg-green-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
             Finish
             </button> -->
-            <button v-if="store.questions.length < 15" 
+            <button v-if="store.questions.length < 7" 
             @click="$router.push('/QuizCreationSummary')"
             type="button" 
             class="text-white bg-green-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">

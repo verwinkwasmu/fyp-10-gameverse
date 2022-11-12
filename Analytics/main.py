39198baeab0de62e3c328d795ed21b58eb2677d7
weixiangtoh <weixiangtoh982@gmail.com>
2022-11-12 14:58:12 +0800

    change graphs to plotly

--- Analytics/main.py
+++ Analytics/main.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import psycopg2
 import altair as alt
+import plotly.graph_objects as go
+import plotly as pt
+
 
 # NOTE: This must be the first command in your app, and must be set only once
 st.set_page_config(layout="wide")
@@ -34,11 +37,19 @@ col1, col2 = st.columns((1, 1))
 with col1:
     # Top 5 players based on total points
     st.write("Top 5 players based on total points:")
-    st.bar_chart(
-        player_df.sort_values(by="total_points", ascending=False).head(5),
-        x="name",
-        y="total_points",
-    )
+
+    top5_players = player_df.sort_values(by="total_points", ascending=False).head(5)
+
+    fig=go.Figure(go.Bar(x=top5_players.name, y=top5_players.total_points,
+                    hovertemplate =
+                    '<b>Player Name</b>: %{x}'+
+                    '<br><b>Total Points</b>: %{y}<extra></extra>',))
+    fig.update_layout(
+        margin=dict(l=0, r=0, t=0, b=50),
+        height=300,
+                 )
+    st.plotly_chart(fig, use_container_width=True)
+
 
 # Second Column
 with col2:
@@ -51,7 +62,15 @@ with col2:
 
     # Top 5 categories based on total points
     st.write("Top 5 Categories played based on total counts:")
-    st.bar_chart(
-        df_1.groupby("category").sum().sort_values(by="count", ascending=False).head(5),
-        y="count",
-    )
+
+    top_categories = df_1.groupby("category").sum().sort_values(by="count", ascending=False).head(5)
+
+    fig1=go.Figure(go.Bar(x=top_categories.index, y=top_categories["count"],
+                        hovertemplate =
+                        '<b>Category</b>: %{x}'+
+                        '<br><b># Times Played</b>: %{y}<extra></extra>',))
+    fig1.update_layout(
+        margin=dict(l=0, r=0, t=0, b=50),
+        height=300,
+                 )
+    st.plotly_chart(fig1, use_container_width=True)
